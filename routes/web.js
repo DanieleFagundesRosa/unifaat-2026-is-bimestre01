75119efc37f05686531d47e67bcca7608ec8838e
Luan Lourenco <luan.tavares.lourenco@gmail.com>
2026-03-23 14:32:17 -0300

    Aula 05 - Cronjobs e Webhooks - webhook path

--- routes/web.js
+++ routes/web.js
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 
 import JsonHelloController from '../app/Http/Controllers/JsonHelloController.js';
+import WebhookHelloController from '../app/Http/Controllers/WebhookController.js';
 
 export default (function () {
 
     const router = Router();
 
+    router.post('/webhook', WebhookHelloController);
+
     router.get('/', JsonHelloController);
 
-    router.post('/webhook', JsonHelloController);
+
 
 
     return router;
