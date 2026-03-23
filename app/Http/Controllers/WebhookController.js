5d356b219676c4dcd1d69628b944663ed2628798
Luan Lourenco <luan.tavares.lourenco@gmail.com>
2026-03-23 14:33:12 -0300

    Aula 05 - Cronjobs e Webhooks - webhook path

--- app/Http/Controllers/WebhookController.js
+++ app/Http/Controllers/WebhookController.js
@@ -2,6 +2,8 @@
 
 export default async function WebhookHelloController(request, response) {
 
+    const HTTP_STATUS = CONSTANTS.HTTP;
+
     console.log("Received webhook request:", request.body);
 
     return response.status(HTTP_STATUS.SUCCESS).json({
